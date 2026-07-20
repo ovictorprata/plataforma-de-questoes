44cc783cc6755694c01a54f8795693ef7d15de19
prata <victorsousaprata@gmail.com>
2026-07-19 21:07:08 -0300

    sobe es lint

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Desativa a exigência do "import React" (desnecessário no React 17+)
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+];
